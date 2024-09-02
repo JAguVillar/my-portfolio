be41ac7b7454dbf8bf79c24c74d3dfeb93882da1
Agustin <jaguvillar@gmail.com>
2024-09-02 12:27:03 -0300

    animacion de carga

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,6 +11,15 @@ export default {
       opacity: {
         "content-theme": "var(--opacity-content-theme)",
       },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn .5s ease-in-out",
+      },
     },
   },
   plugins: [
